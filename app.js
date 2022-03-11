4bdd623df7a73edc22759ebf14cdd4bd17275160
kiran.karki <kiran.karki@student.fh-kiel.de>
2022-03-11 23:14:22 +0100

    defining types in schema

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
 
 const app = express();
+
+const events = [];
+
 const port = 3000;
 
 app.use(bodyParser.json());
@@ -16,11 +19,26 @@ app.use(
     "/graphql",
     graphqlHttp({
         schema: buildSchema(`
+			type Event{
+				_id:ID!
+				title:String! 
+				description:String! 
+				price:Float! 
+				date:String! 
+			}
+
+			input EventInput{
+				title:String! 
+				description:String! 
+				price:Float! 
+				date:String! 
+			}
+
 			type RootQuery{
-				events:[String!]!
+				events:[Event!]!
 			}
 			type RootMutation{
-				createEvent(name:String):String
+				createEvent(eventInput:EventInput):Event
 			}
 			schema{
 				query:RootQuery
@@ -29,11 +47,18 @@ app.use(
 	`),
         rootValue: {
             events: () => {
-                return ["Romantic Cooking", "all night coding"];
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: new Date().toISOString(),
+                };
+                events.push(event);
+                return event;
             },
         }, //bundle of all resolvers
         graphiql: true,
