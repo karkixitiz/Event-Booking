0fe3c8807548d85ef1b628f7ac8c60b33d3cfd19
kiran.karki <kiran.karki@student.fh-kiel.de>
2022-03-13 09:34:22 +0100

    maintaining dependancy between user and event

--- README.md
+++ README.md
@@ -10,7 +10,32 @@ Designing Full stack Event Booking web Application.
 ### React SAP
 
 ### Mongodb
+	- npm install mongoose --save
 ### Node/Express
 	- npm i express bodyParser
 	- npm i --save-dev nodemon
 
+## GraphiQL  Script
+- Get all Events list
+	query{
+	events {
+		title
+		price
+		
+	}
+	}
+
+- Create an Event
+	mutation{createEvent(eventInput:{title:"test",description:"test desc", price:23.99,date:"2020-12-06T09:26:30.645Z"}){
+	title
+	description
+	}
+	}
+
+-Create a User
+	mutation{
+	createUser(userInput:{email:"karkikiran714@gmail.com",password:"test123"}){
+		email
+		password
+	}
+	}
--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
@@ -27,6 +29,12 @@ app.use(
 				price:Float! 
 				date:String! 
 			}
+			 
+			type User{
+				_id:ID!
+				email:String! 
+				password:String 
+			}
 
 			input EventInput{
 				title:String! 
@@ -35,11 +43,17 @@ app.use(
 				date:String! 
 			}
 
+			input UserInput{
+				email:String! 
+				password:String
+			}
+
 			type RootQuery{
 				events:[Event!]!
 			}
 			type RootMutation{
 				createEvent(eventInput:EventInput):Event
+				createUser(userInput:UserInput):User
 			}
 			schema{
 				query:RootQuery
@@ -59,28 +73,57 @@ app.use(
                     });
             },
             createEvent: (args) => {
-                /* const event = {
-				_id: Math.random().toString(),
-				title: args.eventInput.title,
-				description: args.eventInput.description,
-				price: +args.eventInput.price,
-				date: new Date().toISOString(),
-				}; */
-
+                //when creating event, firstly put user id in creator and then update createdEvents in user
                 const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
                     date: new Date(args.eventInput.date),
+                    creator: "622d9dd96fc98cf268864a0e",
                 });
+                let createdEvent;
                 return event
                     .save()
                     .then((result) => {
-                        console.log(result);
-                        return {...result._doc, _id: result._doc._id.toString() }; //spread operator
+                        createdEvent = {...result._doc, _id: result._doc._id.toString() }; //spread operator
+                        return User.findById("622d9dd96fc98cf268864a0e");
+                    })
+                    .then((user) => {
+                        if (!user) {
+                            throw new Error("User not found.");
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then((result) => {
+                        return createdEvent;
                     })
                     .catch((err) => {
-                        console.lgo(err);
+                        throw err;
+                    });
+            },
+            createUser: (args) => {
+                //return promise
+                return User.findOne({
+                        email: args.userInput.email,
+                    })
+                    .then((user) => {
+                        if (user) {
+                            throw new Error("User Exists already.");
+                        }
+                        return bcrypt.hash(args.userInput.password, 12);
+                    })
+                    .then((hashedPassword) => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword,
+                        });
+                        return user.save();
+                    })
+                    .then((result) => {
+                        return {...result._doc, _id: result.id, password: null };
+                    })
+                    .catch((err) => {
                         throw err;
                     });
             },
